b3d0ee864ab75afaa1360a30c48c6c257555937c
Vu Tien Long <tllong20002@gmail.com>
2022-08-13 16:48:07 +0700

    update image user register

--- src/Component/user/SignUp.tsx
+++ src/Component/user/SignUp.tsx
@@ -16,17 +16,17 @@ type Props = {};
 const fromSchema = yup.object().shape({
   username: yup
     .string()
-    .required("Username is required")
-    .min(6, "Username length should be at least 20 characters"),
-  email: yup.string().required("Email is required").email("It not Email"),
+    .required("Không được để trống")
+    .min(6, "Tên quá ngắn"),
+  email: yup.string().required("Không được để trống").email("Email không chính xác"),
   password: yup
     .string()
-    .required("Password is required")
-    .min(6, "Password length should be at least 4 characters"),
+    .required("Không được để trốngd")
+    .min(6, "Mật khẩu quá ngắn"),
   confirmPassword: yup
     .string()
-    .required("Password is required")
-    .oneOf([yup.ref("password")], "Passwords must and should match"),
+    .required("Không được để trống")
+    .oneOf([yup.ref("password")], "Mật khẩu không trùng khớp"),
 });
 const validation = { resolver: yupResolver(fromSchema) };
 
@@ -50,6 +50,7 @@ const SignUp = (props: Props) => {
         username: userForm.username,
         email: userForm.email,
         password: userForm.password,
+        img: "https://i.pinimg.com/564x/04/8c/7e/048c7e0904fe27ffd0b91784a93eacc4.jpg"
       };
       // console.log(user.confirmPassword)
       const { payload } = await dispatch(signUp(user));
@@ -78,24 +79,24 @@ const SignUp = (props: Props) => {
     <div className="box__signup ">
       <div className="signin__form col-span-12 w-10/12 m-auto  flex justify-center">
         <div className="signin__main__right w-full my-24">
-          <h3 className="text-2xl font-bold text-center">Welcome back!</h3>
+          <h3 className="text-2xl font-bold text-center">Chào mừng trở lại !</h3>
           <p className="signin__main__right__text text-center my-8 ">
-            To keep connected with us please login with your personal info
+          Để giữ kết nối với chúng tôi, vui lòng đăng nhập bằng thông tin cá nhân của bạn
           </p>
           <div className="text-center">
-            <NavLink to={"/login"} className="button px-4 border-1 rounded">Sign in</NavLink>
+            <NavLink to={"/login"} className="button px-4 border-1 rounded">Đăng nhập</NavLink>
           </div>
         </div>
         <div className="signin__main__left  w-full ">
           <div className="text-center mr-24">
 
-            <h1 className="text-3xl font-bold">Signin To Website </h1>
-            <div className="signin__main__right__login__google text-center my-6">
+            <h1 className="text-3xl font-bold">Đăng kí</h1>
+            {/* <div className="signin__main__right__login__google text-center my-6">
               <i className="fa-brands fa-facebook" />
               <i className="fa-brands fa-twitter" />
               <i className="fa-solid fa-g" />
-            </div>
-            <p className="text-center">or user email acount</p>
+            </div> */}
+            <p className="text-center">Đăng kí tài khoản mới</p>
             <form onSubmit={handleSubmit(onSubmit)}>
               <div className="m">
                 <input className="px-4 text-white" {...register('username')} type="text" placeholder="Name" />
@@ -121,7 +122,7 @@ const SignUp = (props: Props) => {
               </p>
 
               <div className="text-center">
-                <button className="button px-4 text-white border-1 rounded">Sign up</button>
+                <button className="button px-4 text-white border-1 rounded">Đăng kí</button>
               </div>
 
             </form>
