06e67a6f6c5cb9a522c9fd74b2b6f960e4d0295f
hung15102001 <82076329+hung15102001@users.noreply.github.com>
2022-08-21 21:35:48 +0700

    update welcome

--- src/Component/ModalWelcomwe.tsx
+++ src/Component/ModalWelcomwe.tsx
@@ -8,21 +8,40 @@ const ModalWelcomwe = ({ closeModal }: IModalProps) => {
   return (
 <div className="modal">
       <div className="modal-header">
-        <h2
+        <h4 className='close__modal'
           onClick={() => {
             closeModal(false);
           }}
         >
           X
-        </h2>
+        </h4>
       </div>
       <div className="modal-body">
+        <p className='text__modal'>Bạn có muốn làm test ngay bây giờ ?</p>
         <div>
-            
-            <input type="text" />
-            <input type="text" />
+           <button className='buton__modal yes__modal'>Yes</button>
+           <button  className='buton__modal no__modal'>No</button>
         </div>
       </div>
+      <div className="bg__container__modal">
+    <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" width="100%" height="500px">
+      <path id="shape1" fill-rule="evenodd" fill="rgb(35, 23, 123)" d="M-0.011,9.318 L8.682,6.988 L11.011,15.682 L2.318,18.011 L-0.011,9.318 Z"></path>
+      <path id="shape8" fill-rule="evenodd" fill="rgb(35, 23, 123)" d="M607.694,164.319 L612.680,161.693 L615.306,166.680 L610.320,169.305 L607.694,164.319 Z"></path>
+      <path id="shape9" fill-rule="evenodd" fill="rgb(93, 203, 250)" d="M667.343,205.646 L679.152,213.179 L671.620,224.988 L659.810,217.456 L667.343,205.646 Z"></path>
+      <path id="shape10" fill-rule="evenodd" fill="rgb(93, 203, 250)" d="M915.839,-0.008 L924.997,5.833 L919.156,14.991 L909.998,9.150 L915.839,-0.008 Z"></path>
+      <path id="cir6" fill-rule="evenodd" fill="rgb(35, 23, 123)" d="M989.000,93.000 C993.418,93.000 997.000,96.582 997.000,101.000 C997.000,105.418 993.418,109.000 989.000,109.000 C984.582,109.000 981.000,105.418 981.000,101.000 C981.000,96.582 984.582,93.000 989.000,93.000 Z"></path>
+      <path id="cir5" fill-rule="evenodd" fill="rgb(35, 23, 123)" d="M1165.000,8.000 C1167.761,8.000 1170.000,10.238 1170.000,13.000 C1170.000,15.761 1167.761,18.000 1165.000,18.000 C1162.239,18.000 1160.000,15.761 1160.000,13.000 C1160.000,10.238 1162.239,8.000 1165.000,8.000 Z"></path>
+      <path id="cir4" fill-rule="evenodd" fill="rgb(93, 203, 250)" d="M1303.500,118.000 C1304.881,118.000 1306.000,119.119 1306.000,120.500 C1306.000,121.880 1304.881,123.000 1303.500,123.000 C1302.119,123.000 1301.000,121.880 1301.000,120.500 C1301.000,119.119 1302.119,118.000 1303.500,118.000 Z"></path>
+      <path id="cir3" fill-rule="evenodd" fill="rgb(93, 203, 250)" d="M61.000,112.000 C62.657,112.000 64.000,113.343 64.000,115.000 C64.000,116.657 62.657,118.000 61.000,118.000 C59.343,118.000 58.000,116.657 58.000,115.000 C58.000,113.343 59.343,112.000 61.000,112.000 Z"></path>
+      <path id="cir2" fill-rule="evenodd" fill="rgb(74, 92, 246)" d="M265.500,-0.000 C271.299,-0.000 276.000,4.701 276.000,10.500 C276.000,16.299 271.299,21.000 265.500,21.000 C259.701,21.000 255.000,16.299 255.000,10.500 C255.000,4.701 259.701,-0.000 265.500,-0.000 Z"></path>
+      <path id="cir1" fill-rule="evenodd" fill="rgb(35, 23, 123)" d="M185.500,131.000 C187.985,131.000 190.000,133.015 190.000,135.500 C190.000,137.985 187.985,140.000 185.500,140.000 C183.015,140.000 181.000,137.985 181.000,135.500 C181.000,133.015 183.015,131.000 185.500,131.000 Z"></path>
+    </svg>
+  </div>
+  <div className="box__modal">
+    <div className="wave one"></div>
+    <div className="wave two"></div>
+    <div className="wave three"></div>
+  </div>
     </div>  )
 }
 
--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,220 +1,339 @@
 /* eslint-disable jsx-a11y/alt-text */
 
-import React, { useRef, useState } from 'react'
-import { useForm, SubmitHandler } from 'react-hook-form';
-import { addWellcome } from '../api/welcome';
-import ModalWelcomwe from '../Component/ModalWelcomwe';
+import React, { useRef, useState } from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { addWellcome } from "../api/welcome";
+import ModalWelcomwe from "../Component/ModalWelcomwe";
 import { ReactDimmer } from "react-dimmer";
+import { message } from "antd";
 type FormW = {
-  username:string,
-  numberphone:number,
-  social:string,
-  reason:string,
-  timeStudy:string
-}
+  username: string;
+  numberphone: number;
+  social: string;
+  reason: string;
+  timeStudy: string;
+};
 const Welcome = () => {
-  // const [isModal, setIsModal] = useState(false);
+  const [isModal, setIsModal] = useState(false);
   // const handleClick = () => {
   //   setIsModal((prevState) => !prevState);
   // };
   const {
     register,
     handleSubmit,
-    formState: { errors },} = useForm<FormW>();
-  
-    const onSubmit: SubmitHandler<FormW> = async (product:any) => {
-        try {
-          console.log(product);
-          await addWellcome(product)
-          alert('success')
-          
-        } catch (error) {
-          alert("Lỗi")
-        }
-    }
+    formState: { errors },
+  } = useForm<FormW>();
 
-
-    const reasonSection = useRef(null)
-    const optionForYou = useRef(null)
-    const testlevel = useRef(null)
-    const scrollW = (ref: any) =>{
-      window.scrollTo({
-        top: ref.current.offsetTop,
-        behavior:"smooth"
-      })
+  const onSubmit: SubmitHandler<FormW> = async (product: any) => {
+    const user = localStorage.getItem("user")
+      ? JSON.parse(String(localStorage.getItem("user"))).user.username
+      : "";
+    try {
+      console.log(product);
+      await addWellcome(product);
+      message.success("Thành công");
+      setIsModal((prevState) => !prevState);
+    } catch (error) {
+      message.error("Hệ thống bận");
     }
+  };
+
+  const reasonSection = useRef(null);
+  const optionForYou = useRef(null);
+  const testlevel = useRef(null);
+  const scrollW = (ref: any) => {
+    window.scrollTo({
+      top: ref.current.offsetTop,
+      behavior: "smooth",
+    });
+  };
   return (
     <div>
-
-<div>
-  <form onSubmit={handleSubmit(onSubmit)}>
-  <section className="section-1 ">
-    <p className="title">Bạn biết tới Vogue qua đâu?</p>
-    <div className="box">
-      <div className="row">
-        <div className="col">
-          <img src="../image/image 7.png"  width={140} />
-          <input type="checkbox" id="" value="1" {...register("social")}/>
-          <p>Tìm kiếm Google</p>
-        </div>
-        <div className="col">
-          <img src="../image/image 8.png"  width={140} />
-          <input type="checkbox" id="" value="2" {...register("social")}/>
-          <p>Youtube</p>
-        </div>
-        <div className="col">
-          <img src="../image/image 9.png" width={140}  />
-          <input type="checkbox" id="" value="3" {...register("social")}/>
-          <p>TV</p>
-        </div>
-        <div className="col">
-          <img src="../image/image 10.png" width={140}  />
-          <input type="checkbox"  id="" value="4" {...register("social")}/>
-          <p>Tik Tok</p>
-        </div>
-        <div className="col">
-          <img src="../image/image 11.png" width={140}  />
-          <input type="checkbox" id="" value="5" {...register("social")}/>
-          <p>Facebook &amp;
-            Instagram</p>
-        </div>
-        <div className="col">
-          <img src="../image/image 12.png" width={140}  />
-          <input type="checkbox"  id="" value="6" {...register("social")}/>
-          <p>Bạn bè &amp; Gia đình</p>
-        </div>
-        <div className="col">
-          <img src="../image/image 14.png" width={140}  />
-          <input type="checkbox"  id="" value="7" {...register("social")}/>
-          <p>Báo chí</p>
-        </div>
-        <div className="col">
-          <img src="../image/image 15.png" width={140}  />
-          <input type="checkbox" name="" id="" />
-          <p>Khác</p>
-        </div>
-      </div>
-    </div>
-    <div className="box-btn">
-      <a onClick={()=>scrollW(reasonSection)} className="btn">Tiếp tục</a>
-    </div>
-  </section>
-  <section className="section-1 ">
-    <p className="title" ref={reasonSection}>Tại sao bạn học ngoại ngữ?</p>
-    <div className="box">
-      <div className="row">
-        <div className="col">
-          <img src="../image/image 17.png"  width={140} />
-          <input type="checkbox" id="" value="1" {...register("reason")}/>
-          <p>Tìm kiếm Google</p>
-        </div>
-        <div className="col">
-          <img src="../image/image 18.png"  width={140} />
-          <input type="checkbox" id="" value="2" {...register("reason")}/>
-          <p>Giao tiếp</p>
-        </div>
-        <div className="col">
-          <img src="../image/image 19.png" width={140}  />
-          <input type="checkbox" id="" value="3" {...register("reason")}/>
-          <p>Xem phim</p>
-        </div>
-        <div className="col">
-          <img src="../image/image 20.png" width={140}  />
-          <input type="checkbox" id="" value="4" {...register("reason")}/>
-          <p>Thi cử</p>
-        </div>
-        <div className="col">
-          <img src="../image/image 15.png" width={140}  />
-          <input type="checkbox" id="" value="5" {...register("reason")}/>
-          <p>Khác</p>
-        </div>
-      </div>
-    </div>
-    <div className="box-btn">
-      <a className="btn" onClick={()=>scrollW(optionForYou)}>Tiếp tục</a>
-    </div>
-  </section>
-  <section className="section-2 ">
-    <p className="title" ref={optionForYou}>Chọn mức độ phù hợp với bạn</p>
-    <div className="box">
-      <ul>
-        <li className="rounded-t-xl"><a >
-        <input type="checkbox" id="" value="1" {...register("timeStudy")}/>
-          30 phút/ngày</a></li>
-        <li>
-        <input type="checkbox" id="" value="2" {...register("reason")}/>
-          <a >45 phút/ngày</a></li>
-        <li>
-        <input type="checkbox" id="" value="3" {...register("reason")}/>
-          <a >60 phút/ngày</a></li>
-        <li className="rounded-b-xl">
-        <input type="checkbox" id="" value="4" {...register("reason")}/>
-          <a >90 phút/ngày</a></li>
-          <li className="rounded-b-xl">
-        <input type="checkbox" id="" value="5" {...register("reason")}/>
-          <a >Khác</a></li>
-      </ul>
-    </div>
-    <div className="box-btn">
-      {/* <a className="btn" onClick={()=>scrollW(testlevel)}>Tiếp tục</a> */}
-      <button className="btn">Submit</button>
-    </div>
-  </section>
-
-{/* {isModal && <ModalWelcomwe closeModal={setIsModal} />}
-
-    <ReactDimmer 
-    isOpen={isModal}
-    exitDimmer={setIsModal}
-    zIndex={100}
-    blur={1.5}
-    /> */}
-  </form>
-  <section className="section-3 ">
-    <div className="box">
-      <img src="../image/image 30.png"  />
-    </div>
-    <p className="title" ref={testlevel}>Kiểm tra trình độ tại đây</p>
-    <div className="box-btn mt-[40px] md:mt-[80px] ">
-      <button className="btn">Tiếp tục</button>
-    </div>
-  </section>
-  {/* <section className="section-1 test">
-    <p className="test-name">Đâu là con mèo ?</p>
-    <div className="test-row">
-      <div className="test-row-col">
-        <img src="../image/dog.png"  />
-        <p>1. Dog</p>
-      </div>
-      <div className="test-row-col">
-        <img src="../image/cat.png"  />
-        <p>2. Cat</p>
-      </div>
-      <div className="test-row-col">
-        <img src="../image/birth.png"  />
-        <p>3. Bird</p>
-      </div>
-      <div className="test-row-col">
-        <img src="../image/pig.png"  />
-        <p>3. Pig</p>
-      </div>
-    </div>
-    <div className="test-row-2">
-      <div className="test-row-col-2">
-        <div className="test-btn">
-          <button className="btn">Bỏ qua</button>
-        </div>
-        <div className="test-btn">
-          <button className="btn">Kiểm tra</button>
-        </div>
-      </div>
-    </div>
-  </section> */}
-</div>
+      <div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <section className="section-1">
+            <p className="title">Bạn biết tới Vogue qua đâu?</p>
+            <div className="box">
+              <div className="row">
+                <div className="col social__custom">
+                  <img src="../image/image 7.png" width={140} />
+                  <input
+                    type="checkbox"
+                    id="_checkbox"
+                    className="_checkbox"
+                    value="1"
+                    {...register("social")}
+                  />
+                  <label htmlFor="_checkbox">
+                    <div id="tick_mark"></div>
+                  </label>
+                  <p>Tìm kiếm Google</p>
+                </div>
 
 
-    </div>
-  )
-}
+                <div className="col social__custom">
+                  <img src="../image/image 8.png" width={140} />
+                  <input
+                    type="checkbox"
+                    id="_checkbox2"
+                    className="_checkbox"
+                    value="2"
+                    {...register("social")}
+                  />
+                  <label htmlFor="_checkbox2">
+                    <div id="tick_mark"></div>
+                  </label>
+                  <p>Youtube</p>
+                </div>
+                <div className="col social__custom">
+                  <img src="../image/image 9.png" width={140} />
+                  <input
+                    type="checkbox"
+                    id="_checkbox3"
+                    className="_checkbox"
+                    value="3"
+                    {...register("social")}
+                  />
+                  <label htmlFor="_checkbox3">
+                    <div id="tick_mark"></div>
+                  </label>
+                  <p>TV</p>
+                </div>
+                <div className="col social__custom">
+                  <img src="../image/image 10.png" width={140} />
+                  <input
+                    type="checkbox"
+                    id="_checkbox4"
+                    className="_checkbox"
+                    value="4"
+                    {...register("social")}
+                  />
+                  <label htmlFor="_checkbox4">
+                    <div id="tick_mark"></div>
+                  </label>
+                  <p>Tik Tok</p>
+                </div>
+                <div className="col social__custom">
+                  <img src="../image/image 11.png" width={140} />
+                  <input
+                    type="checkbox"
+                    id="_checkbox5"
+                    className="_checkbox"
+                    value="5"
+                    {...register("social")}
+                  />
+                  <label htmlFor="_checkbox5">
+                    <div id="tick_mark"></div>
+                  </label>
+                  <p>Facebook &amp; Instagram</p>
+                </div>
+                <div className="col social__custom">
+                  <img src="../image/image 12.png" width={140} />
+                  <input
+                    type="checkbox"
+                    id="_checkbox6"
+                    className="_checkbox"
+                    value="6"
+                    {...register("social")}
+                  />
+                  <label htmlFor="_checkbox6">
+                    <div id="tick_mark"></div>
+                  </label>
+                  <p>Bạn bè &amp; Gia đình</p>
+                </div>
+                <div className="col social__custom">
+                  <img src="../image/image 14.png" width={140} />
+                  <input
+                    type="checkbox"
+                    id="_checkbox7"
+                    className="_checkbox"
+                    value="7"
+                    {...register("social")}
+                  />
+                  <label htmlFor="_checkbox7">
+                    <div id="tick_mark"></div>
+                  </label>
+                  <p>Báo chí</p>
+                </div>
+                <div className="col social__custom">
+                  <img src="../image/image 15.png" width={140} />
+                  <input
+                    type="checkbox"
+                    id="_checkbox8"
+                    className="_checkbox"
+                    value="8"
+                    {...register("social")}
+                  />
+                  <label htmlFor="_checkbox8">
+                    <div id="tick_mark"></div>
+                  </label>
+                </div>
+              </div>
+            </div>
+            <div className="box-btn">
+              <a onClick={() => scrollW(reasonSection)} className="btn">
+                Tiếp tục
+              </a>
+            </div>
+          </section>
+          <section className="section-1 ">
+            <p className="title" ref={reasonSection}>
+              Tại sao bạn học ngoại ngữ?
+            </p>
+            <div className="box">
+              <div className="row">
+                <div className="col reason__custom">
+                  <img src="../image/image 17.png" width={140} />
+                  <input
+                    type="checkbox"
+                    id="_reason"
+                    value="1"
+                    className="_checkbox"
+                    {...register("reason")}
+                  />
+                   <label htmlFor="_reason">
+                    <div id="tick_mark"></div>
+                  </label>
+                  <p>Tìm kiếm Google</p>
+                </div>
+                <div className="col reason__custom">
+                  <img src="../image/image 18.png" width={140} />
+                  <input
+                    type="checkbox"
+                    id="_reason2"
+                    value="2"
+                    {...register("reason")}
+                    className="_checkbox"
+                  />
+                   <label htmlFor="_reason2">
+                    <div id="tick_mark"></div>
+                  </label>
+                  <p>Giao tiếp</p>
+                </div>
+                <div className="col reason__custom">
+                  <img src="../image/image 19.png" width={140} />
+                  <input
+                    type="checkbox"
+                    id="_reason3"
+                    value="3"
+                    {...register("reason")}
+                    className="_checkbox"
+                  />
+                   <label htmlFor="_reason3">
+                    <div id="tick_mark"></div>
+                  </label>
+                  <p>Xem phim</p>
+                </div>
+                <div className="col reason__custom">
+                  <img src="../image/image 20.png" width={140} />
+                  <input
+                    type="checkbox"
+                    id="_reason4"
+                    value="4"
+                    {...register("reason")}
+                    className="_checkbox"
+                  />
+                   <label htmlFor="_reason4">
+                    <div id="tick_mark"></div>
+                  </label>
+                  <p>Thi cử</p>
+                </div>
+                <div className="col reason__custom">
+                  <img src="../image/image 15.png" width={140} />
+                  <input
+                    type="checkbox"
+                    id="_reason5"
+                    value="5"
+                    {...register("reason")}
+                    className="_checkbox"
+                  />
+                   <label htmlFor="_reason5">
+                    <div id="tick_mark"></div>
+                  </label>
+                  <p>Khác</p>
+                </div>
+              </div>
+            </div>
+            <div className="box-btn">
+              <a className="btn" onClick={() => scrollW(optionForYou)}>
+                Tiếp tục
+              </a>
+            </div>
+          </section>
+          <section className="section-2 ">
+            <p className="title" ref={optionForYou}>
+              Chọn mức độ phù hợp với bạn
+            </p>
+            <div className="box">
+              <ul>
+                <li className="rounded-t-xl">
+                <input
+                      type="checkbox"
+                      id=""
+                      value="1"
+                      {...register("timeStudy")}
+                    />
+                  <a>
+                    30 phút/ngày
+                  </a>
+                </li>
+                <li>
+                  <input
+                    type="checkbox"
+                    id=""
+                    value="2"
+                    {...register("timeStudy")}
+                  />
+                  <a>45 phút/ngày</a>
+                </li>
+                <li>
+                  <input
+                    type="checkbox"
+                    id=""
+                    value="3"
+                    {...register("timeStudy")}
+                  />
+                  <a>60 phút/ngày</a>
+                </li>
+                <li className="rounded-b-xl">
+                  <input
+                    type="checkbox"
+                    id=""
+                    value="4"
+                    {...register("timeStudy")}
+                  />
+                  <a>90 phút/ngày</a>
+                </li>
+                <li className="rounded-b-xl">
+                  <input
+                    type="checkbox"
+                    id=""
+                    value="5"
+                    {...register("timeStudy")}
+                  />
+                  <a>Khác</a>
+                </li>
+              </ul>
+            </div>
+            <div className="box-btn">
+              {/* <a className="btn" onClick={()=>scrollW(testlevel)}>Tiếp tục</a> */}
+              <button className="btn">Submit</button>
+            </div>
+          </section>
 
-export default Welcome
+          {isModal && <ModalWelcomwe closeModal={setIsModal} />}
+
+          <ReactDimmer
+            isOpen={isModal}
+            exitDimmer={setIsModal}
+            zIndex={100}
+            blur={1.5}
+          />
+        </form>
+      
+      </div>
+    </div>
+  );
+};
+
+export default Welcome;
