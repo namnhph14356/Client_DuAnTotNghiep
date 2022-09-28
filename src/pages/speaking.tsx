b3da3aeac24e3dd2ef21cc5a56e57e7bde240d05
hung15102001 <82076329+hung15102001@users.noreply.github.com>
2022-09-28 13:54:20 +0700

    DTN-140 update page vocab

--- src/pages/speaking.tsx
+++ src/pages/speaking.tsx
@@ -22,15 +22,15 @@ const SpeakingPage = () => {
   const [_category, setTopic] = useState<TopicVocabulary>();
   const [valueLast, setValueLast] = useState<any>();
   const lastElement = {
-    meaning:
-      "Chúc mừng em đã hoàn tất 6 từ vựng của ngày hôm nay. Hãy nhớ phát âm cho chính xác như giọng giáo viên và bất kỳ lúc nào có thời gian, dù đang ở đâu, em hãy nói to từng từ thật nhiều lần cho thật nhuần nhuyễn nhé. Bây giờ, em đã sẵn sàng làm bài tập chưa? Nếu chưa sẵn sàng, hãy bấm vào góc lật trang bên trái để ôn lại các từ của ngày hôm nay nhé. Còn nếu đã sẵn sàng, hãy bấm vào nút “Làm bài tập 1” màu cam để tiếp tục",
-    image: "1.jpg",
+    name: `Chúc mừng em đã hoàn tất ${
+      value.length != null ? value.length : "demo"
+    } từ vựng của ngày hôm nay. Hãy nhớ phát âm cho chính xác như giọng giáo viên và bất kỳ lúc nào có thời gian, dù đang ở đâu, em hãy nói to từng từ thật nhiều lần cho thật nhuần nhuyễn nhé. Bây giờ, em đã sẵn sàng làm bài tập chưa? Nếu chưa sẵn sàng, hãy bấm vào góc lật trang bên trái để ôn lại các từ của ngày hôm nay nhé. Còn nếu đã sẵn sàng, hãy bấm vào nút “Làm bài tập 1” màu cam để tiếp tục`,
+    image:
+      "https://media.istockphoto.com/photos/silhouette-of-a-man-holding-a-trophy-at-sunset-picture-id1202740292?b=1&k=20&m=1202740292&s=612x612&w=0&h=WNQasYubjIswIKcQxq5guDI4z5ca0lttDZDrT3VzmjY=",
     status: 1,
   };
-  const { speaking, supported, voices, speak, resume, cancel, stop, pause } =
-    useSpeechSynthesis();
-  console.log(_category);
-  console.log(value);
+  const { speak } = useSpeechSynthesis();
+  // get id of topic
   const id = "6329714e7cf1a7383fd1e0b9";
   useEffect(() => {
     const getData = async () => {
@@ -38,10 +38,13 @@ const SpeakingPage = () => {
       const valueVocabulary = data.vocabulary;
       setTopic(data?.topic);
       setValue(valueVocabulary);
-      setValueLast({ ...valueVocabulary, lastElement });
+      // Merch slide end
+      setValueLast([...valueVocabulary, lastElement]);
     };
     getData();
   }, []);
+
+  console.log(value.length);
 
   console.log(valueLast);
 
@@ -60,7 +63,7 @@ const SpeakingPage = () => {
             </h2>
           </div>
           <Carousel showIndicators={false}>
-            {value?.map((item, index) => {
+            {valueLast?.map((item, index) => {
               console.log(item.wordForm);
 
               return (
@@ -73,50 +76,72 @@ const SpeakingPage = () => {
                           text: item.words,
                           rate: 0.3,
                           pitch: 0.5,
-                          voices: "en-US",
+                          voices:
+                            "en-US - Microsoft Zira - English (United States)",
                         })
                       }
                     >
                       <i className="fa-solid fa-volume-high"></i>
                     </button>
-                  </div>
 
-                  <div className="info__vocabulry">
                     <h3 className="vocabulary__speaking text-2xl font-bold">
-                      {item.words}
+                      {item.words ? item.words : "Congratulations"} -
+                      <span className="text-gray-400 text-xl">
+                        {item.pa ? item.pa : ""}
+                      </span>
                     </h3>
 
-                    <p className="text-lg font-bold text-center">
-                      (
-                      {item.wordForm === "1"
-                        ? "Nouns"
-                        : item.wordForm === "2"
-                        ? "Adj"
-                        : item.wordForm === "3"
-                        ? "Adv"
-                        : item.wordForm === "4"
-                        ? "Verbs"
-                        : ""}
-                      )
-                    </p>
+                    {/* <div className="right">
+                      [<span className="font-bold text-orange-600 text-xl">
+                        {item.place == 0 ? "UK" : "US"}
+                      </span>]
+                    </div> */}
+
+                    <div className="info__vocabulry">
+                      <span className="text-lg font-bold text-center">
+                      <strong>-</strong>
+                        {item.wordForm === "1"
+                          ? "(Danh từ)"
+                          : item.wordForm === "2"
+                          ? "(Tính từ)"
+                          : item.wordForm === "3"
+                          ? "(Động từ)"
+                          : item.wordForm === "4"
+                          ? "(Verbs)"
+                          : ""}
+                      </span>
+                    </div>
                   </div>
+
                   <div className="flex justify-center">
-                    <div className="w-4/12 ml-4">
+                    <div className="w-4/12 ml-4 mt-6">
+                      <div className="text-2xl text-left font-bold text-orange-600 mt-4">
+                        {item.meaning}
+                      </div>
                       <img
-                        className="image__vocabula w-[100px] h-[150px] rounded-lg"
+                        className="image__vocabula w-[120px] h-[200px] rounded-lg"
                         src={item.image}
                         alt=""
                       />
                     </div>
-                    <div className="w-6/12">
-                      <p className="text-3xl font-bold">{item.meaning}</p>
+                    <div className="w-7/12 my-auto">
+                      {item.example ? (
+                        <p className="text-lg font-bold px-12">
+                          {item.example}
+                        </p>
+                      ) : (
+                        <>
+                          <p className=" font-bold mt-6">{item.name}</p>
+                          <NavLink
+                            to={"/learning/detailLearning/grammar"}
+                            className="font-bold text-white bg-gradient-to-r from-[#6078ea] to-[#17ead9]  rounded p-2 mt-8 border-orange-500 hover:to-[#ff9933] "
+                          >
+                            Next Level
+                          </NavLink>
+                        </>
+                      )}
                     </div>
                   </div>
-                  {/* <div className='box__btn__question'>
-                        <button className='btn__next__question'>
-                            Tiếp tục
-                        </button>
-                    </div> */}
                 </>
               );
             })}
