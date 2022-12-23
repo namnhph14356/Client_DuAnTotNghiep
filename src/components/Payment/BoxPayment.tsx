90388399d72892afe1e2944dd15e39cae9085718
Vu Tien Long <tllong20002@gmail.com>
2022-12-23 23:06:31 +0700

    refactor: update

--- src/components/Payment/BoxPayment.tsx
+++ src/components/Payment/BoxPayment.tsx
@@ -55,7 +55,7 @@ const BoxPayment = (props: Props) => {
         350,000 ĐỒNG / <span className='font-bold text-orange-500'>360 ngày sử dụng</span>
       </p>
       <div className="text-center">
-        {auth.pay == 1 ? <span className='font-bold text-[#ff9933]'>Đã thanh toán</span> : <button className="p-1 bg-red-500 rounded text-white hover:bg-white hover:text-blue-500 border-[1px] border-red-600 font-bold hover:text-red-500" onClick={() => handlonClick()}>Thanh toán</button>}
+        {auth.pay == 1 ? <span className='font-bold text-[#ff9933]'>Đã thanh toán</span> : <button className="p-1 px-3 bg-red-500 rounded text-white hover:bg-white border-[1px] border-red-600 font-bold hover:text-red-500" onClick={() => handlonClick()}>Thanh toán</button>}
 
       </div>
       <div>
--- src/components/Payment/PopupPayment.tsx
+++ src/components/Payment/PopupPayment.tsx
@@ -8,7 +8,7 @@ interface IModalProps {
 const PopupPayment = ({closeModal}: IModalProps) => {
   return (
     <div className="modalPayment">
-      <div className="modal-header">
+      <div className="modal-header" style={{background:"#4F46E5"}}>
         <h4 className='close__modal'
           onClick={() => {
             closeModal(false);
@@ -20,13 +20,12 @@ const PopupPayment = ({closeModal}: IModalProps) => {
       <div className="my-4 text-center">
         <p className='text__modal'>Chọn hình thức thanh toán</p>
         <div className='my-6 flex justify-center gap-3'>
-            <img className='rounded' src={"https://inkythuatso.com/uploads/images/2021/12/vnpay-logo-inkythuatso-01-13-16-26-42.jpg"} alt="" width="40px" height={"40px"} />
-            <NavLink className="p-3 rounded bg-blue-500 text-white font-bold w-[300px] hover:bg-white" to={"/payment/vnpay"}>Thanh toán qua VnPay</NavLink>
+            <img className='rounded' src={"https://inkythuatso.com/uploads/images/2021/12/vnpay-logo-inkythuatso-01-13-16-26-42.jpg"} alt="" width="40px"  />
+            <NavLink className="p-2 rounded bg-blue-500 my-auto text-white font-bold w-[300px] hover:bg-white" to={"/payment/vnpay"}>Thanh toán qua VnPay</NavLink>
         </div>
         <div className='flex justify-center gap-3'>
-            <div><CreditCardOutlined className='text-4xl'/></div>
-            
-            <NavLink className="p-2 rounded bg-blue-500 text-white font-bold w-[300px] hover:bg-white" to={"/payment/qrCode"}>Thanh toán qua chuyển khoản</NavLink>
+            <div><i className="fa-solid fa-credit-card text-4xl my-auto" /></div>
+            <NavLink className="p-2 rounded bg-blue-500 my-auto text-white font-bold w-[300px] hover:bg-white" to={"/payment/qrCode"}>Thanh toán qua chuyển khoản</NavLink>
         </div>
         
       </div>
